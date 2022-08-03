64519e97d41dc3aea905f9b10eb4b59634c715a8
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-03 11:20:21 +0600

    add dom element

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const allHeading2 = document.getElementsByTagName('h2');
+for (const h2 of allHeading2) {
+    console.log(h2.innerHTML)
+}
 
+const allParagraph = document.getElementsByTagName('p');
+for (const p of allParagraph) {
+    console.log(p.innerText);
+}
